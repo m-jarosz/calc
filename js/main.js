e1cbd6401cee5686f30c3ab7c34feaba19c4ecd0
m-jarosz <m.jarosz82@gmail.com>
2022-11-12 11:33:57 +0100

    state after checkout

---
+++ js/main.js
@@ -0,0 +1,75 @@
+(() => {
+    const previousNumber = document.querySelector(".js-previousNumber");
+    const mathOperator = document.querySelector(".js-mathSign");
+    const currentNumber = document.querySelector(".js-currentNumber");
+    const numbers = document.querySelectorAll(".js-sign");
+    const operators = document.querySelectorAll(".js-operator");
+    const equalBtn = document.querySelector(".js-equal");
+    const clearBtn = document.querySelector(".js-clear");
+
+    let result = "";
+
+    const clickNumber = (e) => {
+        if (e.target.textContent === "." && currentNumber.textContent.includes(".")) return;
+        if (e.target.textContent === "." && currentNumber.textContent === "") return currentNumber.textContent = ".0";
+        return currentNumber.textContent += e.target.textContent;
+    }
+
+    const clickOperator = (e) => {
+        if (currentNumber.textContent) {
+            previousNumber.textContent = currentNumber.textContent;
+            currentNumber.textContent = "";
+            return mathOperator.textContent = e.target.textContent;
+        }
+    }
+
+    const showResult = () => {
+        currentNumber.style.fontWeight = "700";
+        currentNumber.style.color = "blue";
+        return currentNumber.textContent = result;
+    }
+
+    const isEqual = () => {
+        let operator = mathOperator.textContent;
+        let a = Number(previousNumber.textContent);
+        let b = Number(currentNumber.textContent);
+        if (typeof a !== "undefined" && typeof b !== "undefined") {
+            switch (operator) {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "x":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = b === 0 ? "n/0 err" : a / b;
+                    break;
+                case "nn":
+                    result = Math.pow(a, b);
+                    break;
+                case "%":
+                    result = (b / 100) * a;
+                    break;
+                case "√":
+                    result = Math.sqrt(a);
+                    break;
+            }
+            showResult();
+        }
+    }
+
+    const clearDisplay = () => {
+        previousNumber.textContent = "";
+        currentNumber.textContent = "";
+        mathOperator.textContent = "";
+        result = "";
+    }
+
+    numbers.forEach((number) => number.addEventListener("click", clickNumber));
+    operators.forEach((operator) => operator.addEventListener("click", clickOperator));
+    equalBtn.addEventListener("click", isEqual);
+    clearBtn.addEventListener("click", clearDisplay);
+})();
